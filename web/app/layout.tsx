6614e489fd6345bee3c85eb910ae2bb437d236c8
Brian Chiwanza <brianchwzdev@gmail.com>
2026-04-21 20:46:02 +0200

    Add Google Search Console verification

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -64,7 +64,7 @@ export const metadata: Metadata = {
   },
   verification: {
     // Add your Google Search Console verification code once you get it:
-    // google: "your-google-verification-code",
+    google: "google2b2ee8074de79b95",
   },
 };
 
